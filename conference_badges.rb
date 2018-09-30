7dd9c8837a4a1511ed326d6ba2730eda5810f5f6
Joseph M Spinelli <josephspinelli@yahoo.com>
2018-09-30 19:09:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
+def batch_badge_creator(speakers)
   speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace" , "Linus", "Matz"]
   speakers.each do |name|
     "Here is #{name}."
